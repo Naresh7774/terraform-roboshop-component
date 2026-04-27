5bcea9145225c3ee79036b9a58b76588a9e3ca6f
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-27 20:57:38 +0530

    project

--- data.tf
+++ data.tf
@@ -7,3 +7,14 @@ data "aws_ami" "joindevops" {
         values = ["RHEL-9-DevOps-Practice"]
     }
 
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
