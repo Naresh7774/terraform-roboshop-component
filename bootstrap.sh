efbfbb6f39a37054a32f996e274544fd95d29785
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-27 20:57:12 +0530

    project

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,3 +12,17 @@ mkdir -p $REPO_DIR
 mkdir -p /var/log/roboshop/
 touch ansible.log
 
+cd $REPO_DIR
+
+# check if ansible repo is already cloned or not
+
+if [ -d $ANSIBLE_DIR ]; then
+
+    cd $ANSIBLE_DIR
+    git pull
+else
+    git clone $REPO_URL
+    cd $ANSIBLE_DIR
+fi
+echo "environment is: $2"
+ansible-playbook -e component=$component -e env=$environment main.yaml
