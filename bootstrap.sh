9ec7ff1c6f313371961d705190e1decaa684e14d
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-27 20:56:59 +0530

    project

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,8 @@ dnf install ansible -y
 REPO_URL=https://github.com/daws-86s/ansible-roboshop-roles-tf.git
 REPO_DIR=/opt/roboshop/ansible
 ANSIBLE_DIR=ansible-roboshop-roles-tf
+
+mkdir -p $REPO_DIR
+mkdir -p /var/log/roboshop/
+touch ansible.log
+
