2841489aeceb8f4120f7c40a6c344ebd89643cdf
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-28 23:11:23 +0530

    project

--- main.tf
+++ main.tf
@@ -180,3 +180,13 @@ resource "aws_autoscaling_policy" "main" {
     target_value = 75.0
   }
 }
+
+
+resource "aws_lb_listener_rule" "main" {
+  listener_arn = local.listener_arn
+  priority     = var.rule_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
